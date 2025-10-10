3e82a352f156be4c238d109f7a47e343569b0b87
CallePalsson <carlerikuno@gmail.com>
2025-10-10 13:35:01 +0200

    Add: Basic code to admin.cs doctor.cs patient.cs
    removed User.cs

---
+++ doctor.cs
@@ -0,0 +1,29 @@
+namespace App;
+
+public class Doctor : IUser
+{
+    public string Username;
+    public string Password;
+    public bool IsLoggedIn;
+
+    public Doctor(string username, string password, bool isloggedin)
+    {
+        Username = username;
+        Password = password;
+
+        IsLoggedIn = isloggedin;
+    }
+    public bool TryLogin(string username, string password)
+    {
+        if (username == Username && password == Password)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+//temporär klass, ska bli fler som ex admin, läkare, pasient
---
+++ patient.cs
@@ -0,0 +1,29 @@
+namespace App;
+
+public class Patient : IUser
+{
+    public string Username;
+    public string Password;
+    public bool IsLoggedIn;
+
+    public Patient(string username, string password, bool isloggedin)
+    {
+        Username = username;
+        Password = password;
+
+        IsLoggedIn = isloggedin;
+    }
+    public bool TryLogin(string username, string password)
+    {
+        if (username == Username && password == Password)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+//temporär klass, ska bli fler som ex admin, läkare, pasient
