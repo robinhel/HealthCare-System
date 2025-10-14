cc2faa8656601f162e5a718a0315ed1cd860a467
onicklaas <github nicklaas-@hotmail.com>
2025-10-14 16:22:43 +0200

    Created methods in participant class
    To use in program.cs

--- Participant.cs
+++ Participant.cs
@@ -2,13 +2,25 @@ namespace App;
 
 class Participant
 {
-    public User user;
-    public UserRole userRole;
-    public 
+    public string name;
+    public EventRole eventRole;
 
-
-}
-enum EventRole
-{
+    public Participant(string name, EventRole eventRole)
+    {
+        this.name = name;    // göra det tydligt vilken som e klass och vilken som är klassens fält och parametrar
+        this.eventRole = eventRole;
+    }
     
+    public void ShowUser()
+    {
+        Console.WriteLine($"Hello {name}, your role is {eventRole}");
+    }
 }
+public enum EventRole
+{
+    Doctor,
+    Patient,
+}
+
+//Participant p1 = new Participant("Nicklas", EventRole.Doctor);
+// Participant p2 = new Participant("Erik", EventRole.Doctor);
