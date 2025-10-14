6a46b7cb989f57edd6e1c211eee9256203bf6e66
robinhel <hellgrenrobin@gmail.com>
2025-10-14 10:32:49 +0200

    Add: Events class

---
+++ Event.cs
@@ -0,0 +1,27 @@
+namespace App;
+
+public class Event
+{
+
+    List<Event> events = new List<Event>();
+    List<Event> participents = new List<Event>();
+
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+
+
+    public Event(string title, string description, string location, DateTime start, DateTime end)
+    {
+        Title = title;
+        Description = description;
+        Location = location;
+        Start = start;
+        End = end;
+    }
+
+
+}
